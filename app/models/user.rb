f1c545f718e909ddb363745f8167ebd0443f759a
pramod-wdo <pramodhc007@gmail.com>
2025-07-05 00:25:43 +0530

    adds seeds value using faker gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  PROFILE_TITLE = [
+    'Senior Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Senior Full Stack Ruby on Rails Developer',
+    'Junior Full Stack Ruby on Rails Developer',
+    'Senior Java Developer',
+    'Senior Front End Developer'
+  ].freeze       
 end
